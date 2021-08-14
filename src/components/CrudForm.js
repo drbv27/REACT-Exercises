53acc3b8f94297ba09801e19ea9b475ab9403b7f
Diego <drbv27@gmail.com>
2021-08-14 05:55:19 -0500

    Implementing the U functionality on fake CRUD

--- src/components/CrudForm.js
+++ src/components/CrudForm.js
@@ -8,6 +8,14 @@ const initialForm = {
 
 const CrudForm = ({ createData, updateData, dataToEdit, setDataToEdit }) => {
   const [form, setForm] = useState(initialForm);
+
+  useEffect(() => {
+    if (dataToEdit) {
+      setForm(dataToEdit);
+    } else {
+      setForm(initialForm);
+    }
+  }, [dataToEdit]);
   const handleChange = (e) => {
     setForm({
       ...form,
@@ -37,7 +45,7 @@ const CrudForm = ({ createData, updateData, dataToEdit, setDataToEdit }) => {
   };
   return (
     <div>
-      <h3>Agregar</h3>
+      <h3>{dataToEdit ? "Editar" : "Agregar"}</h3>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
