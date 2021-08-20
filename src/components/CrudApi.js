a983e91b85c7a26dfcff7209c487bae2f7c92575
Diego <drbv27@gmail.com>
2021-08-20 10:07:49 -0500

    Using the helper

--- src/components/CrudApi.js
+++ src/components/CrudApi.js
@@ -1,30 +1,95 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import { helpHttp } from "../helpers/helpHttp";
 import CrudForm from "./CrudForm";
 import CrudTable from "./CrudTable";
+import Loader from "./Loader";
+import Message from "./Message";
 
 const CrudApi = () => {
-  const [db, setDb] = useState([]);
+  const [db, setDb] = useState(null);
   const [dataToEdit, setDataToEdit] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  let api = helpHttp();
+  let url = "http://localhost:3000/santos";
+
+  useEffect(() => {
+    setLoading(true);
+    helpHttp()
+      .get(url)
+      .then((res) => {
+        //console.log(res);
+        if (!res.err) {
+          setDb(res);
+          setError(null);
+        } else {
+          setDb(null);
+          setError(res);
+        }
+        setLoading(false);
+      });
+  }, [url]);
 
   const createData = (data) => {
-    console.log(data);
     data.id = Date.now();
-    setDb([...db, data]);
+    //console.log(data);
+
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+
+    api.post(url, options).then((res) => {
+      //console.log(res);
+      if (!res.err) {
+        setDb([...db, res]);
+      } else {
+        setError(res);
+      }
+    });
   };
 
   const updateData = (data) => {
-    let newData = db.map((el) => (el.id === data.id ? data : el));
-    setDb(newData);
+    let endpoint = `${url}/${data.id}`;
+    //console.log(endpoint);
+
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+
+    api.put(endpoint, options).then((res) => {
+      //console.log(res);
+      if (!res.err) {
+        let newData = db.map((el) => (el.id === data.id ? data : el));
+        setDb(newData);
+      } else {
+        setError(res);
+      }
+    });
   };
 
   const deleteData = (id) => {
     let isDelete = window.confirm(
-      `¿Estas seguro de eliminar el registro con el id '${id}'?`
+      `¿Estás seguro de eliminar el registro con el id '${id}'?`
     );
+
     if (isDelete) {
-      let newData = db.filter((el) => el.id !== id);
-      setDb(newData);
+      let endpoint = `${url}/${id}`;
+      let options = {
+        headers: { "content-type": "application/json" },
+      };
+
+      api.del(endpoint, options).then((res) => {
+        //console.log(res);
+        if (!res.err) {
+          let newData = db.filter((el) => el.id !== id);
+          setDb(newData);
+        } else {
+          setError(res);
+        }
+      });
     } else {
       return;
     }
@@ -32,7 +97,7 @@ const CrudApi = () => {
 
   return (
     <div>
-      <h2>CRUD App</h2>
+      <h2>CRUD API</h2>
       <article className="grid-1-2">
         <CrudForm
           createData={createData}
@@ -40,11 +105,20 @@ const CrudApi = () => {
           dataToEdit={dataToEdit}
           setDataToEdit={setDataToEdit}
         />
-        <CrudTable
-          data={db}
-          setDataToEdit={setDataToEdit}
-          deleteData={deleteData}
-        />
+        {loading && <Loader />}
+        {error && (
+          <Message
+            msg={`Error ${error.status}: ${error.statusText}`}
+            bgColor="#dc3545"
+          />
+        )}
+        {db && (
+          <CrudTable
+            data={db}
+            setDataToEdit={setDataToEdit}
+            deleteData={deleteData}
+          />
+        )}
       </article>
     </div>
   );
